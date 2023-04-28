a27f373673e3cef8f792f78c011e8d01c2648ea4
atakan18 <atakankasbasi@gmail.com>
2023-04-28 22:33:19 +0300

    Api Consume

---
+++ AkademiPlusApi.ConsumeLayer/Controllers/CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using AkademiPlusApi.ConsumeLayer.Models.CustomerViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AkademiPlusApi.ConsumeLayer.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CustomerController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44385/api/Customer");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<CustomerListViewModel>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
